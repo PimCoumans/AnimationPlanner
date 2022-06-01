5dbb4d7d8a0a8603904f794caacfa6294179fc5e
Pim <pim@pixelrock.nl>
2022-06-01 18:13:28 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AnimationSequence
+
+A description of this package.
---
+++ Sources/AnimationSequence/AnimationSequence.swift
@@ -0,0 +1,6 @@
+public struct AnimationSequence {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
