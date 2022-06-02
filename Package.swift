a98e70f992458df54c9ed13ea05df47a76e7b589
Pim <pim@pixelrock.nl>
2022-06-02 18:04:07 +0200

    5.3 is more than enough

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -22,7 +22,5 @@ let package = Package(
             name: "AnimationPlannerTests",
             dependencies: ["AnimationPlanner"]),
     ],
-    swiftLanguageVersions: [
-        .v5
-    ]
+    swiftLanguageVersions: [.v5]
 )
