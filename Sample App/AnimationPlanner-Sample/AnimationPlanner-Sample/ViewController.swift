63391e11ca85ed6f5b8afdb155bbb024dc84f1df
Pim <pim@pixelrock.nl>
2022-06-03 15:32:17 +0200

    Added sneaky copy, way too much effort

--- Sample App/AnimationPlanner-Sample/AnimationPlanner-Sample/ViewController.swift
+++ Sample App/AnimationPlanner-Sample/AnimationPlanner-Sample/ViewController.swift
@@ -82,6 +82,9 @@ extension ViewController {
     }
     
     func runComplexAnimation() {
+        
+        var sneakyCopy: UIView! // Don‘t worry, you‘ll see later
+        
         UIView.animateSteps { sequence in
             let quarterHeight = view.bounds.height / 4
             let view = setInitialSubviewState()
@@ -99,34 +102,46 @@ extension ViewController {
                 }
                 .add(duration: 1, timingFunction: .cubicInOut) {
                     view.frame.origin.y += quarterHeight
-                }
-                .add(duration: 0.1, options: .curveEaseInOut) {
                     view.transform = .identity
-                    view.backgroundColor = .systemBlue
                 }
-                .delay(1)
+                .delay(0.32)
             
-            // Adding multiple steps from a loop is pretty
-            // straightforward
-            (0..<4).map({ CGFloat($0) / 4 }).forEach { offset in
+            // Adding multiple steps from a loop is pretty straightforward
+            (1...4).map({ CGFloat($0) / 4 }).forEach { offset in
+                let reversed = 1 - offset
+                let initialCornerRadius = view.layer.cornerRadius
                 sequence.add(duration: 0.2, timingFunction: .backOut) {
                     view.transform = CGAffineTransform(
                         rotationAngle: .pi * offset
                     ).scaledBy(
-                        x: 1 + offset,
-                        y: 1 + offset)
-                    view.alpha = 1 - (offset / 2)
+                        x: 1 + offset / 2,
+                        y: 1 + offset / 2)
+                    view.layer.cornerRadius = initialCornerRadius * reversed
                 }
                 .delay(0.25)
             }
             
             // Continue the chain again by calling the next step on the sequence object
             sequence
+                .add(duration: 0.01, animations: {
+                    view.transform = view.transform.rotated(by: .pi) // reset rotation
+                })
+                .add(duration: 0.01, animations: {
+                    sneakyCopy = view.sneakyCopy()
+                })
+                // Example of using a custom extension (defined further down) for a specific animation
+                .shake(view)
+                .add(duration: 0.25, timingFunction: .backOut, animations: {
+                    sneakyCopy.isHidden = false
+                    sneakyCopy.transform = CGAffineTransform(translationX: 0, y: -view.frame.height - 20)
+                    sneakyCopy.backgroundColor = .systemYellow
+                })
                 .delay(0.35)
-                .add(duration: 1, timingFunction: .quadInOut) {
+                .add(duration: 1.2, timingFunction: .quartInOut) {
                     view.transform = .identity
-                    view.frame.origin.y += quarterHeight
-                    view.alpha = 1
+                    let offset = view.frame.origin.y + quarterHeight
+                    view.frame.origin = CGPoint(x: view.frame.minX - (view.frame.width / 2) - 10, y: offset)
+                    sneakyCopy.frame = view.frame.offsetBy(dx: view.frame.width + 20, dy: 0)
                     view.backgroundColor = .systemPink
                 }
                 .delay(0.5)
@@ -135,28 +150,83 @@ extension ViewController {
                     // finishing when the longest animation completes
                     // Use a delay for a staggered effect
                     group.animate(duration: 0.5, delay: 0.2, timingFunction: .backOut) {
-                        view.transform = CGAffineTransform(translationX: 50, y: 0)
+                        sneakyCopy.transform = CGAffineTransform(translationX: 0, y: -50).concatenating(sneakyCopy.transform)
                     }
                     .animate(duration: 0.2, delay: 0.1, timingFunction: .cubicOut) {
-                        view.layer.borderColor = UIColor.systemBlue.cgColor
+                        view.layer.borderColor = view.backgroundColor?.cgColor
                         view.layer.borderWidth = 4
+                        sneakyCopy.layer.borderColor = sneakyCopy.backgroundColor?.cgColor
+                        sneakyCopy.layer.borderWidth = 4
                     }
                     .animate(duration: 1) {
-                        view.backgroundColor = .systemBlue
+                        let viewColor = view.backgroundColor
+                        view.backgroundColor = sneakyCopy.backgroundColor
+                        sneakyCopy.backgroundColor = viewColor
                     }
                 }
                 .delay(0.32)
                 .add(duration: 0.5, timingFunction: .quintIn) {
                     view.alpha = 0
+                    sneakyCopy?.alpha = 0
                     // you can use values set in previous animations
                     // as the animations are created after the previous animation completes
                     view.transform = view.transform.translatedBy(x: 0, y: quarterHeight)
+                    sneakyCopy?.transform = view.transform.translatedBy(x: 0, y: quarterHeight)
                 }
         } completion: { finished in
             // Let‘s watch that again, shall we?
             self.runComplexAnimation()
         }
         
+    }
+}
+
+extension AnimationSequence {
+    func shake(_ view: UIView) -> Self {
+        var baseTransform: CGAffineTransform = .identity
+        add(duration: 0) {
+            // Get the current transform of the view right before applying random offset
+            baseTransform = view.transform
+        }
+        let count = 50
+        let maxRadius: CGFloat = 4
+        let moveButton: (_ index: Int) -> Void = { index in
+            let radius = CGFloat(index) / CGFloat(count) * maxRadius
+            self.add(duration: 0.015, timingFunction: .quintInOut) {
+                view.transform = baseTransform
+                    .translatedBy(
+                        x:CGFloat.random(in: -radius...radius),
+                        y: CGFloat.random(in: -radius...radius)
+                    )
+            }
+        }
+        (0...count).forEach(moveButton)
+//        (0...count).map { count - $0 }.forEach(moveButton)
+        return self
+    }
+}
+
+extension UIView {
+    func sneakyCopy() -> Self? {
+        do {
+            let archiver = NSKeyedArchiver(requiringSecureCoding: false)
+            archiver.encodeRootObject(self)
+            let data = archiver.encodedData
+            
+            let unarchiver = try NSKeyedUnarchiver(forReadingFrom: data)
+            unarchiver.requiresSecureCoding = false
+
+            guard let view = unarchiver.decodeObject() as? Self else {
+                return nil
+            }
+            view.isHidden = true
+            superview?.insertSubview(view, belowSubview: self)
+            return view
+        }
+        catch {
+            print(error)
+            return nil
+        }
     }
 }
 
