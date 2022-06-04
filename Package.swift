868f1d12191ae8094cad081cd9889b883c3513a7
Pim <pim@pixelrock.nl>
2022-06-04 11:46:20 +0200

    Adding DocC support (#3)

--- Package.swift
+++ Package.swift
@@ -24,3 +24,9 @@ let package = Package(
     ],
     swiftLanguageVersions: [.v5]
 )
+#if swift(>=5.6)
+  // Add the documentation compiler plugin if possible
+  package.dependencies.append(
+    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
+  )
+#endif
